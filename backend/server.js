2f2647ed58a9c073bf24564c268623d536f1f687
Talha Ergüney <talhaergy@gmail.com>
2025-07-31 14:11:47 +0300

    fix: production env düzenlendi

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,10 @@ const PORT = process.env.PORT || 5001;
 const allowedOrigins = [
   'http://localhost:5174',
   'https://job-tracking-blue.vercel.app',
-  'https://job-tracking-git-main-talha-erguneys-projects.vercel.app'
+  'https://job-tracking-git-main-talha-erguneys-projects.vercel.app',
+  'https://job-tracking-befd.onrender.com' // ✅ Eklendi
 ];
+
 
 app.use(cors({
   origin: function (origin, callback) {
